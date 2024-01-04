990273e8dbf70dbe08bce69c37a1622a2e4812ce
DESKTOP-50U56HO <kamranvi@code.edu.az>
2024-01-04 15:52:51 +0400

    Implementing Material UI into admin panel on UI side

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { AdminModule } from './admin/admin.module';
 import { UiModule } from './ui/ui.module';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     AdminModule,
     UiModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
