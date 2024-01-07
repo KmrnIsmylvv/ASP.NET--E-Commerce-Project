6ca5e755b6efdd2364b62e527b3d6b0f9ba781d4
DESKTOP-50U56HO <kamranvi@code.edu.az>
2024-01-07 20:47:08 +0400

    Added alertifyjs package and created AlertifyService on the UI side

---
+++ Client/src/app/services/admin/alertify.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+declare var alertify: any;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertifyService {
+
+  constructor() { }
+
+  message(message: string, options: Partial<AlertifyOptions>) {
+    alertify.set('notifier', 'delay', options.delay)
+    alertify.set('notifier', 'position', options.position);
+    const msg = alertify[options.messageType](message);
+
+    if (options.dismissOthers)
+      msg.dismissOthers();
+  }
+
+  dismiss() {
+    alertify.dismissAll();
+  }
+}
+
+export class AlertifyOptions {
+  messageType: MessageType = MessageType.Message;
+  position: Position = Position.BottomRight;
+  delay: number = 3;
+  dismissOthers: boolean = false;
+}
+
+export enum MessageType {
+  Error = "error",
+  Message = "message",
+  Notify = "notify",
+  Success = "success",
+  Warning = "warning"
+}
+
+export enum Position {
+  TopLeft = 'top-left',
+  TopCenter = 'top-center',
+  TopRight = 'top-right',
+  BottomLeft = 'bottom-left',
+  BottomCenter = 'bottom-center',
+  BottomRight = 'bottom-right'
+}
