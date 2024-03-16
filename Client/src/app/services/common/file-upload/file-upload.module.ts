d5b60151b79d50b68696afa2b01fedd550ca1401
DESKTOP-50U56HO <kamranvi@code.edu.az>
2024-03-16 23:05:14 +0400

    Update angular version

--- Client/src/app/services/common/file-upload/file-upload.module.ts
+++ Client/src/app/services/common/file-upload/file-upload.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NgxFileDropModule } from 'ngx-file-drop';
-import { FileUploadComponent } from './file-upload.component';
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { NgxFileDropModule } from 'ngx-file-drop';
+// import { FileUploadComponent } from './file-upload.component';
 
 
 
-@NgModule({
-  declarations: [
-    FileUploadComponent,
-  ],
-  imports: [
-    CommonModule,
-    NgxFileDropModule
-  ],
-  exports: [
-    FileUploadComponent
-  ],
-})
-export class FileUploadModule { }
+// @NgModule({
+//   declarations: [
+//     FileUploadComponent,
+//   ],
+//   imports: [
+//     CommonModule,
+//     NgxFileDropModule
+//   ],
+//   exports: [
+//     FileUploadComponent
+//   ],
+// })
+// export class FileUploadModule { }
